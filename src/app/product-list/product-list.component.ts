b5d95626c48c690f8c87d4de63e3487c5162d2fb
Matej Koprivec <matej.koprivec@outlook.com>
2024-11-14 21:27:08 +0100

    Add actions buttons to product-list table

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [MatTableModule, MatIconModule, MatButtonModule],
+  imports: [MatTableModule, MatIconModule, MatButtonModule, MatSortModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent implements OnInit {
   #productservice = inject(ProductService);
   products: Product[] = [];
+  dataSource: MatTableDataSource<Product> = new MatTableDataSource();
   displayedColumns: string[] = ['id', 'product_name', 'logo', 'created_by', 'modified_by', 'modified_on', 'languages', 'action'];
 
   ngOnInit(): void {
-    this.#productservice.getProducts().subscribe(res => this.products = res);
+    this.#productservice.getProducts().subscribe(res => this.dataSource = new MatTableDataSource(res));
   }
 }
